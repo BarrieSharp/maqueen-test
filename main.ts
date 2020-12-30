27144e9d17b68da9a368b6ba8ac0bf9fc132942e
Barrie <barrie.sharp@hotmail.co.uk>
2020-12-30 14:02:30 +0000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,12 +1,22 @@
+// Using the speaker
 input.onButtonPressed(Button.A, function () {
-    music.playMelody("A G A F G E F D ", 120)
+    Play_Melody = true
+    while (Play_Melody) {
+        music.playMelody("A G A F G E F D ", 200)
+    }
 })
 input.onGesture(Gesture.Shake, function () {
+    Play_Melody = false
     music.stopAllSounds()
+})
+// Display IR messages
+maqueen.IR_callbackUser(function (IRmessage) {
+    basic.showNumber(IRmessage)
 })
 let BLUE = 0
 let GREEN = 0
 let RED = 0
+let Play_Melody = false
 let RGB_Light = neopixel.create(DigitalPin.P15, 4, NeoPixelMode.RGB)
 basic.showLeds(`
     . # # # .
@@ -15,10 +25,7 @@ basic.showLeds(`
     # . # . #
     . # # # .
     `)
-// Display light level
-basic.forever(function () {
-    basic.showNumber(input.lightLevel())
-})
+// Colour changing ambient LEDs
 basic.forever(function () {
     RED = 0
     GREEN = 0
@@ -41,4 +48,8 @@ basic.forever(function () {
         basic.pause(1)
         RGB_Light.showColor(neopixel.rgb(RED, GREEN, BLUE))
     }
+})
+// Display light level
+basic.forever(function () {
+    basic.showNumber(input.lightLevel())
 })
